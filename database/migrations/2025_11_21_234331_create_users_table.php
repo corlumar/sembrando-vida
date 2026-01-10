58ddc9b0a3498db286530f596fbc88d54f5f1b48
Esteban Lopez <corlumar@hotmail.com>
2026-01-09 18:26:45 -0600

    Create full users table schema in migration

--- database/migrations/2025_11_21_234331_create_users_table.php
+++ database/migrations/2025_11_21_234331_create_users_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->string('apellido_paterno', 150)->nullable();
+            $table->string('apellido_materno', 150)->nullable();
+            $table->string('curp', 18)->unique()->nullable();
+            $table->string('email')->unique();
+            $table->string('celular', 20)->unique()->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
